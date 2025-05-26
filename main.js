5a440883b832b884cc3ab6096ab99e1b1e460298
Elizabeth González <79029943+Artemisse99@users.noreply.github.com>
2025-05-25 20:12:30 -0500

    Add files of the me portfolio
    Add files of the portfolio by Elizabeth Gonzalez

---
+++ main.js
@@ -0,0 +1,30 @@
+
+const projectLinks = [
+    "https://www.jaclinicalresearch.com/",
+    "https://www.energymedicalcenters.com/",
+    "https://arac22.com/",
+    "https://www.paradisusmedresearch.com/",
+    "https://www.makyalstudio.com/",
+    "https://www.odontologiasoma.com/",
+    "https://luzhelena.co/",
+    "https://cempu.edu.co/",
+    "https://tvgcare.com/",
+    "https://palacemetal.com/",
+    "https://ceramicasmoderna.co/",
+    "https://distrielectricas.com.co/",
+    "https://embellecer.com.co/",
+    "https://cleanservices.co/",
+    "https://lcservices.com.co/",
+    "https://hotelversalles.com.co/",
+    "https://www.colchonesroyalflex.com.co/",
+    "https://www.viaccesorios.com.co/"
+];
+
+const container = document.querySelector(".projects .grid");
+projectLinks.forEach(link => {
+    const card = document.createElement("div");
+    card.className = "project-card";
+    const title = link.replace(/https?:\/\//, "").replace("www.", "").split(".")[0];
+    card.innerHTML = `<h3>${title}</h3><a href="${link}" target="_blank">Visitar sitio</a>`;
+    container.appendChild(card);
+});
